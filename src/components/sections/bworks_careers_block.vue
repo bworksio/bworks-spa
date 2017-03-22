eb9d28d501131985dfc86e730830932a6e848d6d
Stefan M. Kudwien <mail@stefankudwien.com>
2017-03-22 18:00:36 +0100

    Cosmetic fixes: make fade transitions globally work again, fix logo link triggers page reload

--- src/components/sections/bworks_careers_block.vue
+++ src/components/sections/bworks_careers_block.vue
@@ -95,12 +95,5 @@
         text-align: center;
       }
     }
-
-    .fade-enter-active, .fade-leave-active {
-      transition: opacity .5s
-    }
-    .fade-enter, .fade-leave-to {
-      opacity: 0
-    }
   }
 </style>
