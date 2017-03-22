4f1cb74f92934b0c9ef106ec5e0bace959bbee82
Stefan M. Kudwien <mail@stefankudwien.com>
2017-03-22 18:04:21 +0100

    Fix inverted header not reset when navigating to a page without ‚basic page‘ header

--- src/components/sections/bworks_basic_page.vue
+++ src/components/sections/bworks_basic_page.vue
@@ -61,7 +61,7 @@
     },
     beforeDestroy () {
       // Also destroy ScrollMagic scene.
-      smHeaderInvertScene.destroy()
+      smHeaderInvertScene.destroy(true)
     },
     components: {
       AppLanguageSwitcher
