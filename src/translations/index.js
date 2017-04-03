5810507086e700a0c37c83903ad7006c6cbbd870
Stefan M. Kudwien <mail@stefankudwien.com>
2017-04-03 18:19:39 +0200

    BW-70 Translate menu toggle

--- src/translations/index.js
+++ src/translations/index.js
@@ -1,12 +1,16 @@
 export default {
   en: {
     button: {
+      menu: 'Menu',
+      close: 'Close',
       hire_us: 'Hire us',
       apply_now: 'Apply now'
     }
   },
   de: {
     button: {
+      menu: 'Menü',
+      close: 'Schliessen',
       hire_us: 'Anfrage senden',
       apply_now: 'Jetzt bewerben'
     }
