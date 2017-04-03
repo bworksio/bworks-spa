b2aeae68e8168cb290016ede045a72a2b26b4f60
Stefan M. Kudwien <mail@stefankudwien.com>
2017-04-03 14:27:39 +0200

    BW-70 Add loading animation (video currently disabled)

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,13 @@
     <transition name="fade">
       <div v-if="isLoading" id="preloading">
         <div v-html="assets.bWorksPreloading" class="logo"></div>
+        <!--video playsinline autoplay poster="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" style="">
+          <source src="static/Bworks-logoanimation.webm" type="video/webm">
+          <source src="static/Bworks-logoanimation.mp4" type="video/mp4">
+        </video-->
+        <transition name="fade" appear>
+          <div v-if="!error" class="loading delay">Loading</div>
+        </transition>
         <transition name="fade">
           <div v-if="error" class="error">{{ error }}</div>
         </transition>
@@ -224,6 +231,11 @@
     background: #fff;
     z-index: 999;
 
+    video {
+      width: 100%;
+      height: 100%;
+    }
+
     .logo {
       height: 100%;
       display: flex;
@@ -235,11 +247,32 @@
       }
     }
 
+    .loading,
     .error {
       position: absolute;
       top: 75%;
       width: 100%;
       text-align: center;
+    }
+
+    .loading:after {
+      overflow: hidden;
+      display: inline-block;
+      vertical-align: bottom;
+      animation: ellipsis steps(4,end) 900ms infinite;
+      content: "\2026"; /* ascii code for the ellipsis character */
+      width: 0;
+      margin-right: 1.25em;
+    }
+
+    @keyframes ellipsis {
+      to {
+        width: 1.25em;
+        margin-right: 0;
+      }
+    }
+
+    .error {
       color: red;
     }
   }
