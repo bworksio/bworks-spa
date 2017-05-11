d40895412b4c110b2d8fd4ee6185a526695193ac
Stefan M. Kudwien <mail@stefankudwien.com>
2017-05-11 13:13:09 +0200

    Make markers interactive

--- src/components/helpers/ContactMap.vue
+++ src/components/helpers/ContactMap.vue
@@ -1,7 +1,7 @@
 <template>
-  <gmap-map :center="{lat: 47.1, lng: 9.9}" :zoom="7" :options="{scrollwheel: false, styles}" >
-    <gmap-marker :position="{lat: 47.3664, lng: 8.4921}"></gmap-marker>
-    <gmap-marker :position="{lat: 46.4999, lng: 11.3497}"></gmap-marker>
+  <gmap-map :center="{lat: 47.1, lng: 9.9}" :zoom="7" :options="options">
+    <gmap-marker :position="{lat: 47.3664, lng: 8.4921}" :clickable="true" @click="go('https://www.google.com/maps/dir/Birmensdorferstrasse+541,+8055+Z%C3%BCrich,+Schweiz/')"></gmap-marker>
+    <gmap-marker :position="{lat: 46.4999, lng: 11.3497}" :clickable="true" @click="go('https://www.google.com/maps/dir/Museumstrasse+41A,+39100+Bozen,+Italien/')"></gmap-marker>
   </gmap-map>
 </template>
 
@@ -21,88 +21,101 @@
 
     data () {
       return {
-        // @see https://snazzymaps.com/editor/customize/25
-        /* eslint-disable quotes */
-        styles: [
-          {
-            "featureType": "administrative",
-            "elementType": "labels.text.fill",
-            "stylers": [
-              {
-                "color": "#444444"
-              }
-            ]
-          },
-          {
-            "featureType": "landscape",
-            "elementType": "all",
-            "stylers": [
-              {
-                "color": "#f2f2f2"
-              }
-            ]
-          },
-          {
-            "featureType": "poi",
-            "elementType": "all",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "road",
-            "elementType": "all",
-            "stylers": [
-              {
-                "saturation": -100
-              },
-              {
-                "lightness": 45
-              }
-            ]
-          },
-          {
-            "featureType": "road.highway",
-            "elementType": "all",
-            "stylers": [
-              {
-                "visibility": "simplified"
-              }
-            ]
-          },
-          {
-            "featureType": "road.arterial",
-            "elementType": "labels.icon",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "transit",
-            "elementType": "all",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "water",
-            "elementType": "all",
-            "stylers": [
-              {
-                "color": "#4b7fe2"
-              },
-              {
-                "visibility": "on"
-              }
-            ]
-          }
-        ]
+        options: {
+          scrollwheel: false,
+          mapTypeControl: false,
+          streetViewControl: false,
+          minZoom: 7,
+          maxZoom: 13,
+          // @see https://snazzymaps.com/editor/customize/25
+          /* eslint-disable quotes */
+          styles: [
+            {
+              "featureType": "administrative",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#444444"
+                }
+              ]
+            },
+            {
+              "featureType": "landscape",
+              "elementType": "all",
+              "stylers": [
+                {
+                  "color": "#f2f2f2"
+                }
+              ]
+            },
+            {
+              "featureType": "poi",
+              "elementType": "all",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "road",
+              "elementType": "all",
+              "stylers": [
+                {
+                  "saturation": -100
+                },
+                {
+                  "lightness": 45
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "all",
+              "stylers": [
+                {
+                  "visibility": "simplified"
+                }
+              ]
+            },
+            {
+              "featureType": "road.arterial",
+              "elementType": "labels.icon",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "transit",
+              "elementType": "all",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "water",
+              "elementType": "all",
+              "stylers": [
+                {
+                  "color": "#4b7fe2"
+                },
+                {
+                  "visibility": "on"
+                }
+              ]
+            }
+          ]
+        }
+      }
+    },
+
+    methods: {
+      go (url) {
+        window.open(url, '_blank')
       }
     },
 
