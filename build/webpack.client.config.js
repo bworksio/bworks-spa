2be2608821c5d2f87c813f57d2322f907bcff145
Stefan M. Kudwien <mail@stefankudwien.com>
2018-07-23 10:10:39 +0200

    Add back webpack-define-plugin
    We still need it for VUE_ENV, and also for NODE_ENV until https://github.com/vuejs/vue-loader/pull/1345 has been resolved.

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -11,12 +11,10 @@ const config = merge(base, {
   },
   plugins: [
     // strip dev-only code in Vue source
-/*
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
       'process.env.VUE_ENV': '"client"'
     }),
-*/
     // extract vendor chunks for better caching
 /*
     new webpack.optimize.CommonsChunkPlugin({
