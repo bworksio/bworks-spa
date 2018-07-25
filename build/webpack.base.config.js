03a579062624ce68b71672a19200f01732ab725c
Stefan M. Kudwien <mail@stefankudwien.com>
2018-07-25 09:56:10 +0200

    Output named chunks

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -13,7 +13,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../dist'),
     publicPath: '/dist/',
-    filename: '[name].[chunkhash].js'
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js'
   },
   resolve: {
     extensions: ['.wasm', '.mjs', '.js', '.json', '.vue'],
