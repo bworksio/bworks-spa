6ab4bebbd5a2e48b7b04c146f985dac76a7039d0
Ilya Ershov <xp47xp34@gmail.com>
2019-09-23 14:35:50 +0600

    Added events for GA for routing, projects' scrolling, mail and phones links

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import 'es6-promise/auto'
+import jQuery from 'jquery'
 import { createApp } from './app'
-
 // a global mixin that calls `asyncData` when a route component's params change
 Vue.mixin({
   beforeRouteUpdate (to, from, next) {
@@ -16,15 +16,12 @@ Vue.mixin({
     }
   }
 })
-
 const { app, router, store } = createApp()
-
 // prime the store with server-initialized state.
 // the state is determined during SSR and inlined in the page markup.
 if (window.__INITIAL_STATE__) {
   store.replaceState(window.__INITIAL_STATE__)
 }
-
 // wait until router has resolved all async before hooks
 // and async components...
 router.onReady(() => {
@@ -43,18 +40,45 @@ router.onReady(() => {
     if (!asyncDataHooks.length) {
       return next()
     }
-
     Promise.all(asyncDataHooks.map(hook => hook({ app, route: to })))
       .then(() => {
         next()
       })
       .catch(next)
   })
-
+  // Google analytics for router
+  router.beforeEach(function (to, from, next) {
+    app.$ga.event({
+      eventCategory: 'router',
+      eventAction: 'click',
+      eventLabel: 'Router link click',
+      eventValue: {
+        from: from.path,
+        to: to.path
+      }
+    })
+    next()
+  })
   // actually mount to DOM
   app.$mount('#app')
 })
-
+// Google analytics for email anf phones links
+jQuery(document).on('click', 'a[href^="mailto:"]', function () {
+  app.$ga.event({
+    eventCategory: 'Email link',
+    eventAction: 'click',
+    eventLabel: 'Email link click',
+    eventValue: jQuery(this).attr('href').substr(7)
+  })
+})
+jQuery(document).on('click', 'a[href^="tel:"]', function () {
+  app.$ga.event({
+    eventCategory: 'Phone link',
+    eventAction: 'click',
+    eventLabel: 'Phone link click',
+    eventValue: jQuery(this).attr('href').substr(4)
+  })
+})
 // service worker
 // if ('https:' === location.protocol && navigator.serviceWorker) {
 //   navigator.serviceWorker.register('/service-worker.js')
