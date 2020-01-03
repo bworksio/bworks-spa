9596b26c7cc908f4e5876cc8227e44de08e2facd
Spiros <souliotis@bizr.gr>
2020-01-03 15:53:12 +0100

    bump

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get('*', isProd ? render : (req, res) => {
   readyPromise.then(() => render(req, res))
 })
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 37878
 app.listen(port, () => {
   console.log(`server started at localhost:${port}`)
 })
